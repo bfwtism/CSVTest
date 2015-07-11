6e82f6e03c950c21abf28e6a14e9c4e1833ebe5f
fabian <fabwat85@hotmail.com>
2015-07-11 19:43:50 +0100

    Commit 4

---
+++ src/main/java/Import.java
@@ -0,0 +1,21 @@
+package fabian.watson.csvtest;
+
+//import csv
+//prompt user for file location
+//prompt user for first data row, and if there is a header
+// /Users/fabian/OneDrive/1.\ Working/Project\ 1505\ Java/Beijing_2013_HourlyPM2.5.csv 
+
+
+//constructor
+public class Import {
+
+}
+
+//methods
+
+//main method
+
+public void main(){
+	
+}
+
